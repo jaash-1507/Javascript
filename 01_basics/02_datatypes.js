9df2a2eaa9ab295f9ce7826b22633918825759e9
jaash-1507 <134307843+jaash-1507@users.noreply.github.com>
2024-03-14 06:52:33 +0000

    datatypes done video 5

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+// alert("Hello") This can be used only in browers
+
+console.log(3+3);
+
+let name = "Jash"
+let age = 20
+let isLogin = false
+
+console.table([name,age,isLogin]);
+
+
+//Primitive datatypes
+//number
+//boolean
+//bigint
+//string = " "
+//null   standalone value
+//undefined
+//symbol
+
+
+//object
+
+console.log(typeof "jash");
+console.log(typeof null);
+console.log(typeof undefined);
