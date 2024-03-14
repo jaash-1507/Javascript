25a174955559b3dfad48aacc008356d310c8b9b1
jaash-1507 <134307843+jaash-1507@users.noreply.github.com>
2024-03-14 06:34:35 +0000

    variable types done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 28361032
+let accountEmail = "jash@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+//accountId = 2 //Not allowed
+accountEmail = "jpanchal@gmail.com"
+accountPassword = "jash"
+accountCity = "Mumbai"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
