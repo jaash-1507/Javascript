c8a53474d1469aca0c0488ae75e78cda4d17fbda
jaash-1507 <134307843+jaash-1507@users.noreply.github.com>
2024-03-16 03:53:16 +0000

    comparisons of operands

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,10 @@
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// === strictly check : also checks the datatype of the operands
+console.log("2" === 2);
