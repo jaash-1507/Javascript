a95f60870ab2b36bad23466149c4cce5e44df3c5
jaash-1507 <134307843+jaash-1507@users.noreply.github.com>
2024-03-16 03:44:33 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,10 +1,13 @@
+
+// ***********************Conversions******************
+
 let score = "33abc"
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
 
 /*
 "33" => 33
@@ -14,8 +17,16 @@ true => 1; false => 0
 
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1 => true; 0=>false
 //""=>false;
 //"hitesh"=>true
+
+
+/********************Operations*********************/
+
+
+let str1 = "hello"
+let str2 = "jash"
+console.log(str1 + str2);
