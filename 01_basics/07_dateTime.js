78bc69956906fe98ed62ada563d17b262aa945a6
jaash-1507 <134307843+jaash-1507@users.noreply.github.com>
2024-03-17 06:25:58 +0000

    date and time

---
+++ 01_basics/07_dateTime.js
@@ -0,0 +1,10 @@
+//Dates
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON);
+console.log(myDate.toLocaleDateString());
+console.log(myDate.getHours);
+console.log(typeof myDate);
