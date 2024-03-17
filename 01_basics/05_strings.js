ac59e5d0e39bfa7f6952cf8bb18201868b96ee47
jaash-1507 <134307843+jaash-1507@users.noreply.github.com>
2024-03-17 05:52:13 +0000

    strings in js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const name = "jash"
+const repoCount = 30
+
+//console.log(name + repoCount);
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`);
+
+const gameName = new String('jashpanchal');
+console.log(gameName);
