0376a8c2f48f235e04804022158e9d67d53289e3
jaash-1507 <134307843+jaash-1507@users.noreply.github.com>
2024-03-17 06:11:19 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,21 @@
+/***********Numbers***************/
+
+const balance = new Number(100);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(3));
+
+const score = 40.987;
+
+//console.log(score.toPrecision(4));
+
+const hundreds = 100000
+//console.log(hundreds.toLocaleString());
+//console.log(hundreds.toLocaleString('en-IN'));
+
+/**********Maths*************/
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.random());
